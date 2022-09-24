1d27b9040108dba82addb868da1fdd8ac9f02f56
Dastan <cdos@MacBook-Pro-Dastan.local>
2022-09-25 05:31:51 +0600

    spline_interpolation

--- www/api/v2/client/views.py
+++ www/api/v2/client/views.py
@@ -4,6 +4,7 @@ from api.v2.client.serializers import ProfileSerializer, EventSerializer, ShortD
 from core.models import Profile, Event, DataSet
 from external_api import interpolate_road_points
 from rest_framework.response import Response
+from scipy import interpolate
 
 
 # --- Views ---
@@ -52,41 +53,69 @@ class InterpolatedDataSetAPIView(RetrieveAPIView):
     def retrieve(self, request, *args, **kwargs):
         queryset = self.queryset.get(pk=kwargs['pk'])
         data = {'identifier': queryset.identifier, 'data_instances': []}
-        interpolate = []
+        if queryset.data_instances.count() == 1:
+            data['data_instances'].append(DataInstanceSerializer(queryset.data_instances.first()).data)
+            return Response(data)
+        interpolate1 = []
+        x, y, z, t = [], [], [], []
         for instance in queryset.data_instances.all():
-            if instance.activity == 'driving':
-                interpolate.append(f'{instance.lat},{instance.lng}')
-            elif len(interpolate) != 0:
-                interpolated_points = interpolate_road_points('|'.join(interpolate))
-                for point in interpolated_points:
-                    data['data_instances'].append({
-                        'lat': point['latitude'],
-                        'lng': point['longitude'],
-                        'alt': 0,
-                        'identifier': None,
-                        'timestamp': 0,
-                        'floor': None,
-                        'horizontal_accuracy': 0,
-                        'vertical_accuracy': 0,
-                        'confidence': 0.0,
-                        'activity': 'driving',
-                    })
-                interpolate = []
-            if instance.activity != 'driving':
-                data['data_instances'].append(DataInstanceSerializer(instance).data)
-        if len(interpolate) != 0:
-            interpolated_points = interpolate_road_points('|'.join(interpolate))
-            for point in interpolated_points:
-                data['data_instances'].append({
-                    'lat': point['latitude'],
-                    'lng': point['longitude'],
-                    'alt': 0,
-                    'identifier': None,
-                    'timestamp': 0,
-                    'floor': None,
-                    'horizontal_accuracy': 0,
-                    'vertical_accuracy': 0,
-                    'confidence': 0.0,
-                    'activity': 'driving',
-                })
+            x.append(instance.lat)
+            y.append(instance.lng)
+            z.append(instance.alt)
+            t.append(instance.timestamp)
+        #     if instance.activity == 'driving':
+        #         interpolate.append(f'{instance.lat},{instance.lng}')
+        #     elif len(interpolate) != 0:
+        #         interpolated_points = interpolate_road_points('|'.join(interpolate))
+        #         for point in interpolated_points:
+        #             data['data_instances'].append({
+        #                 'lat': point['latitude'],
+        #                 'lng': point['longitude'],
+        #                 'alt': 0,
+        #                 'identifier': None,
+        #                 'timestamp': 0,
+        #                 'floor': None,
+        #                 'horizontal_accuracy': 0,
+        #                 'vertical_accuracy': 0,
+        #                 'confidence': 0.0,
+        #                 'activity': 'driving',
+        #             })
+        #         interpolate = []
+        #     if instance.activity != 'driving':
+        #         data['data_instances'].append(DataInstanceSerializer(instance).data)
+        # if len(interpolate) != 0:
+        #     interpolated_points = interpolate_road_points('|'.join(interpolate))
+        #     for point in interpolated_points:
+        #         data['data_instances'].append({
+        #             'lat': point['latitude'],
+        #             'lng': point['longitude'],
+        #             'alt': 0,
+        #             'identifier': None,
+        #             'timestamp': 0,
+        #             'floor': None,
+        #             'horizontal_accuracy': 0,
+        #             'vertical_accuracy': 0,
+        #             'confidence': 0.0,
+        #             'activity': 'driving',
+        #         })
+        sx = interpolate.InterpolatedUnivariateSpline(t, x)
+        sy = interpolate.InterpolatedUnivariateSpline(t, y)
+        sz = interpolate.InterpolatedUnivariateSpline(t, z)
+        start_time = t[0]
+        end_time = t[-1]
+        dt = (t[-1] - t[0]) // 100
+        while start_time < end_time:
+            data['data_instances'].append({
+                'lat': sx(start_time),
+                'lng': sy(start_time),
+                'alt': sz(start_time),
+                'identifier': None,
+                'timestamp': start_time,
+                'floor': None,
+                'horizontal_accuracy': 0,
+                'vertical_accuracy': 0,
+                'confidence': 0.0,
+                'activity': 'driving',
+            })
+            start_time += dt
         return Response(data)
